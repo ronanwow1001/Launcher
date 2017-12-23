86663b61e0606753a5ff54b4c1442dd1c0247300
Ben <ibapple4@gmail.com>
2017-12-23 17:12:55 -0500

    Dynamic lookup of resources

--- Corporate Clash/Core/Audio.cs
+++ Corporate Clash/Core/Audio.cs
@@ -5,24 +5,24 @@ namespace CorporateClash.Core
 {
 	public static class Audio
 	{
+	    public const string Click = "sndclick";
+
 		public static void PlaySoundFile(string filename)
 		{
 			try
 			{
 				if (!Settings.Instance.WantClickSound) return;
-				switch (filename.ToLower())
-				{
-					case "sndclick":
-						{
-							Log.Info("Playing sound click");
-							new SoundPlayer(Properties.Resources.sndclick).Play();
-							break;
-						}
-				}
-			}
-			catch (Exception ex)
+
+                // Search for the sound
+                var sound = Properties.Resources.ResourceManager.GetStream(filename);
+
+                // Play sound
+			    Log.Info($"Playing {filename}");
+			    new SoundPlayer(sound).Play();
+            }
+            catch (Exception ex)
 			{
-				Log.Error("UNABLE TO PLAY SOUND");
+				Log.Error($"Unable to play audio: {filename}");
 				Log.Error(ex);
 			}
 		}
